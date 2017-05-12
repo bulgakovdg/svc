654cd0a0f8bb378edf57b2b1a972023e9e56a811
bulgakovdg <bulgakovdg@TSK-L-BULGAKOV.bankrc.local>
2017-05-12 17:06:15 +0700

    Фикса 3

--- src/main/java/TestServlet1.java
+++ src/main/java/TestServlet1.java
@@ -4,22 +4,29 @@
  * and open the template in the editor.
  */
 
-import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  *
  * @author bulgakovdg
  */
-@WebServlet(urlPatterns = {"/TestServlet"})
-public class TestServlet extends HttpServlet {
+@WebServlet(urlPatterns = {"/TestServlet1"})
+public class TestServlet1 extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -38,10 +45,10 @@ public class TestServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet TestServlet</title>");            
+            out.println("<title>Servlet TestServlet1</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet TestServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet TestServlet1 at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -59,11 +66,11 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        processRequest(request, response);
-    String nextJSP = "/DoFoto.html";
-    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
-    dispatcher.forward(request,response);
+        String nextJSP = "/DoFoto.html";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+        dispatcher.forward(request,response);
     }
+
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -75,7 +82,56 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+Connection conn;
+                                PrintWriter out;
+                            out = response.getWriter();
+                            out.println("123-1");
+
+
+		try {
+			DataSource dataSource = null;
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/worldDB");
+			
+//			Class.forName("com.mysql.jdbc.Driver");
+//			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MySQL?verifyServerCertificate=false&useSSL=true", "root", "root");
+			conn = dataSource.getConnection();
+			String query = "insert into myschema.user_data(login, password, email) values('ddd', 'sss', 'gfdgd')";
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			System.out.println("2");
+                        String znak = null;
+			while (rs.next()) {
+				System.out.println(rs.getString("login"));
+                                
+				znak = rs.getString("login");        
+
+			}
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet TestServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet TestServlet at " + znak + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+                            out.println("123-2");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+                        out.println("123-3");
+
+		}   		
+      
     }
 
     /**
--- src/main/java/ru/sovcombank/fotoreport/SaveFotoServlet.java
+++ src/main/java/ru/sovcombank/fotoreport/SaveFotoServlet.java
@@ -95,5 +95,3 @@ public class SaveFotoServlet extends HttpServlet {
     }
     
 }
-
-}
